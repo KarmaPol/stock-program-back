cd38cd91ef9f647cbe84015bbab292ecc55377c1
KarmaPol <kkhkr98@gmail.com>
2023-11-07 18:09:23 +0900

    feat : project init

---
+++ src/main/java/com/pgms/stockprogramback/StockProgramBackApplication.java
@@ -0,0 +1,13 @@
+package com.pgms.stockprogramback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockProgramBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockProgramBackApplication.class, args);
+    }
+
+}
