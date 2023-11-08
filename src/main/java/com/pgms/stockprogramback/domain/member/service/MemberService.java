6b1e06695c0e759022fb00e90ae51480d74dba08
KarmaPol <kkhkr98@gmail.com>
2023-11-08 13:25:58 +0900

    feat : 전체 stock 조회, 멤버 보유 stock 조회

--- src/main/java/com/pgms/stockprogramback/domain/member/service/MemberService.java
+++ src/main/java/com/pgms/stockprogramback/domain/member/service/MemberService.java
@@ -2,22 +2,31 @@ package com.pgms.stockprogramback.domain.member.service;
 
 import com.pgms.stockprogramback.domain.member.model.Member;
 import com.pgms.stockprogramback.domain.member.repository.MemberRepository;
+import com.pgms.stockprogramback.domain.memberStock.dto.MemberStockResponseDto;
+import com.pgms.stockprogramback.domain.memberStock.mapper.MemberStockMapper;
+import com.pgms.stockprogramback.domain.memberStock.mapper.MemberStockMapperImpl;
+import com.pgms.stockprogramback.domain.stock.mapper.StockMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final MemberStockMapper memberStockMapper;
+    private final StockMapper stockMapper;
 
     public Member getMember(Long id){
         return memberRepository.findById(id).orElseThrow(() -> new RuntimeException("존재하지 않는 멤버입니다."));
     }
 
-    public void getMemberStockByMember(Long memberId){
+    public List<MemberStockResponseDto> getMemberStockByMember(Long memberId){
         Member member = getMember(memberId);
-//        member.getMemberStocks().stream().map(s -> )
+        return member.getMemberStocks().stream()
+                .map(s -> memberStockMapper.memberStockToMemberStockResponseDto(s, stockMapper.stockToStockResponseDto(s.getStock()))).toList();
     }
 }
--- src/main/java/com/pgms/stockprogramback/domain/stock/service/StockService.java
+++ src/main/java/com/pgms/stockprogramback/domain/stock/service/StockService.java
@@ -1,11 +1,14 @@
 package com.pgms.stockprogramback.domain.stock.service;
 
+import com.pgms.stockprogramback.domain.stock.dto.StockResponseDto;
+import com.pgms.stockprogramback.domain.stock.mapper.StockMapper;
 import com.pgms.stockprogramback.domain.stock.model.Stock;
 import com.pgms.stockprogramback.domain.stock.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,8 +17,13 @@ import java.util.Optional;
 public class StockService {
 
     private final StockRepository stockRepository;
+    private final StockMapper stockMapper;
 
     public Stock getStock(Long id){
         return stockRepository.findById(id).orElseThrow(() -> new RuntimeException("존재하지 않는 Stock입니다"));
+    }
+
+    public List<StockResponseDto> getStockList(){
+        return stockRepository.findAll().stream().map(stockMapper::stockToStockResponseDto).toList();
     }
 }
